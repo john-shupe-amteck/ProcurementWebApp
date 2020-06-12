1dc8183b88c8da53d5bd3b147bd544ef28a9a987
JShupe <jshupe@amteck.com>
2020-06-12 11:16:28 -0400

    Learning about canvas and rendering

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 'use strict';
+const canvas = document.querySelector('.myCanvas');
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+const ctx = canvas.getContext('2d');
+
+function loop() {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+    ctx.fillRect(0, 0, width, height);
+  
+    for(let i = 0; i < balls.length; i++) {
+      balls[i].draw();
+      balls[i].update();
+      balls[i].collisionDetect();
+    }
+  
+    requestAnimationFrame(loop);
+  }
+  
+  loop();
