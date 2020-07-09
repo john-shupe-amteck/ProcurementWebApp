d504d868298393ba62e9a84f45d184185457e82f
John-Shupe-Amteck <jshupe@amteck.com>
2020-07-09 11:18:52 -0400

    added total budget cost

--- php/job-page/budgeted-amounts-query.php
+++ php/job-page/budgeted-amounts-query.php
@@ -2,8 +2,12 @@
 $query = "SELECT
     A.description,
     sum(quantity) as qty,
-    `unit-cost` 
+    avg(`unit-cost`) as 'unit-cost',
+    `cost-unitID`,
+    `cost-units`.`conversion`,
+    sum(quantity) * avg(`unit-cost`) * `cost-units`.`conversion` as total
   FROM `budget-details` A
+  LEFT JOIN `cost-units` on `cost-units`.ID = A.`cost-unitID`
   WHERE
     A.budgetID in (
       SELECT B.ID 
@@ -24,6 +28,7 @@ echo "
       <th>Description</th>
       <th class='budgeted-amount-quantity'>Quantity   </th>
       <th class='budgeted-amount-cost'    >Cost       </th>
+      <th class='budgeted-amount-total'   >Total      </th>
     </tr>
   </thead>
   <tbody>";
@@ -33,7 +38,8 @@ echo '
     <tr>
     <td class="monospace">'.                          $row["description"]             .'</td>
     <td class="budgeted-amount-quantity monospace" style="text-align:right">'. number_format($row["qty"])      .'</td>
-    <td class="budgeted-amount-cost monospace" style="text-align:right">'. number_format($row["unit-cost"]).'</td>
+    <td class="budgeted-amount-cost monospace" style="text-align:right">$'. number_format($row["unit-cost"]).'/'.$row["cost-unitID"].'</td>
+    <td class="budgeted-amount-total monospace" style="text-align:right">'. number_format($row["total"]).'</td>
     </tr>';
 }
 echo "
