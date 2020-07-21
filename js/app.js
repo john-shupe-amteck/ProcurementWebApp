778ead04953a94a0f5ab7b0f4d722af1d000eed5
John-Shupe-Amteck <jshupe@anteck.com>
2020-07-21 16:01:53 -0400

    jquery now does sub table

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
-function toggle(id) {
-  var x = document.getElementById(id);
-  if (x.style.visibility === "collapse") {
-    x.style.visibility = "visible";
-  }  else {
-    x.style.visibility = "collapse";
-  }
-}
+$(function() {
+  // hides all PO tables on load
+  $(".sub-table").each(function(e) {
+    $(this).hide();
+  })
+
+  // Colors expired equipment rentals red
+  $(".finish-date").each(function (index, element) {
+    var a = new Date($(this).text());
+    var b = new Date();
+    
+    if (a < b) {
+      $(this).css("color", "red");
+    }  
+  });
+
+  // toggles visibility of PO tables
+  $(".main-info").click(function (e) { 
+    $(this).next().toggle(200);    
+  });
+})
